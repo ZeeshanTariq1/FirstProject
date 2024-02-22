f4c453c96ef1d2eea79a06e1df07e24fc8025bff
ZeeshanTariq1 <160941722+ZeeshanTariq1@users.noreply.github.com>
2024-02-23 00:20:16 +0500

    Add files via upload

---
+++ CircleTest.java
@@ -0,0 +1,9 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        double radius = 2.00;
+        System.out.println("area of circle  " + c.calArea(radius));
+        System.out.println("perimeter of circle  " + c.calPerimeter(radius));
+
+    }
+}
